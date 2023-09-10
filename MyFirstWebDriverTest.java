a9025a19a5e6161ce67935c1c6605cb4ee0282ae
Ketan Jobanputra <ketanvj@yahoo.com>
2023-09-10 15:08:41 +0530

    changed maximize to fullScreen in main branch

--- MyFirstWebDriverTest.java
+++ MyFirstWebDriverTest.java
@@ -11,7 +11,7 @@ public class MyFirstWebDriverTest {
 	public static void main(String[] args) {
 //This line added in main branch
 		WebDriver driver = new ChromeDriver(); // Opens the chrome browser
-		driver.manage().window().maximize();
+		driver.manage().window().fullScreen();
 		driver.get("https://nichethyself.com/tourism/home.html");
 		WebElement userName = driver.findElement(By.name("username"));
 		userName.sendKeys("stc123");
